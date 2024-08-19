69e15eebc3bb252d58974e74f300c71ead378d40
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2024-08-18 21:00:35 -0700

    Commit circulos
    Utiliza beginPath, arc y stroke

--- operaciones.js
+++ operaciones.js
@@ -1,33 +1,52 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-//Cuadrados
-ctx.fillStyle = "red";
-ctx.fillRect(20, 20, 100, 100);
-
-ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
-ctx.fillRect(110, 105, 100, 100);
-
-ctx.fillStyle = "green";
-ctx.fillRect(200, 190, 100, 100);
-
-ctx.strokeStyle = "black";  
-ctx.lineWidth = 2;
-ctx.strokeRect(290, 275, 100, 100);
-
-ctx.strokeStyle = "red";  
-ctx.lineWidth = 3;
-ctx.strokeRect(370, 355, 100, 100);
-
-
-//Triangulo
+//CIRCULOS
 ctx.beginPath();
-ctx.moveTo(320, 255);    
-ctx.lineTo(430, 150);   
-ctx.lineTo(320, 500);   
+ctx.arc(150, 400, 120, 0, 2 * Math.PI);
+ctx.fillStyle = "orange";             
+ctx.fill(); 
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(290, 400, 120, 0, 2 * Math.PI);
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(580, 420, 40, 1.1, Math.PI);
+ctx.fillStyle = "orange";             
+ctx.fill(); 
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2;
+ctx.stroke();
+
+//TRIANGULO
+ctx.beginPath();
+ctx.moveTo(750, 205); 
+ctx.lineTo(950, 205); 
+ctx.lineTo(850, 130);  
 ctx.closePath();      
-ctx.fillStyle = "rgba(0, 255, 255, 0.5)"; 
-ctx.fill();              
-ctx.strokeStyle = "red";
+
+ctx.fillStyle = "rgba(31, 147, 241)";
+ctx.fill();            
+
+//CUADRADOS
+var moverHorizontal = 200;
+ctx.strokeStyle = "rgba(90, 90, 90)";
 ctx.lineWidth = 3;
-ctx.stroke();            
+ctx.strokeRect(160 + moverHorizontal, 105, 100, 100);
+
+ctx.fillStyle = "black";
+ctx.fillRect(210 + moverHorizontal, 155, 100, 100);  
+
+ctx.fillStyle = "rgba(31, 147, 241)";  
+ctx.fillRect(265 + moverHorizontal, 205, 100, 100);
+
+ctx.strokeStyle = "lightgreen";  
+ctx.lineWidth = 1;
+ctx.strokeRect(315 + moverHorizontal, 255, 100, 100);
