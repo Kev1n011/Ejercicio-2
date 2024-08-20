2db379990a3b53497f1f61e61a1940eb3de49107
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2024-08-19 22:17:54 -0700

    Commit ejercicio 4
    Utiliza beginPath, arc y stroke

--- operaciones.js
+++ operaciones.js
@@ -16,37 +16,48 @@ ctx.strokeStyle = "black";
 ctx.lineWidth = 2.5;
 ctx.stroke();
 
-
 ctx.beginPath();
-ctx.arc(580, 420, 40, 1.1, Math.PI);
-ctx.fillStyle = "orange";             
-ctx.fill(); 
-ctx.strokeStyle = "black";
-ctx.lineWidth = 2;
-ctx.stroke();
+ctx.arc(290, 330, 120, .8, 2.4); // Arco para la sonrisa
+ctx.fillStyle = "black"; // Color de relleno
+ctx.fill(); // Rellenar la forma
+ctx.strokeStyle = "blue"; // Color de borde
+ctx.lineWidth = 3; // Ancho de la línea de borde
+ctx.stroke(); // Dibujar el borde
 
-//TRIANGULO
+//TRIANGULOS
+   
+
+var moverLados = 100;
 ctx.beginPath();
-ctx.moveTo(750, 205); 
-ctx.lineTo(950, 205); 
-ctx.lineTo(850, 130);  
+ctx.moveTo(320 + moverLados, 255);    
+ctx.lineTo(430 + moverLados, 150);   
+ctx.lineTo(320 + moverLados, 500);   
 ctx.closePath();      
+ctx.fillStyle = "rgba(0, 255, 255, 0.5)"; 
+ctx.fill();              
+ctx.strokeStyle = "red";
+ctx.lineWidth = 3;
+ctx.stroke();        
 
-ctx.fillStyle = "rgba(31, 147, 241)";
-ctx.fill();            
+          
 
 //CUADRADOS
-var moverHorizontal = 200;
-ctx.strokeStyle = "rgba(90, 90, 90)";
+//Cuadrados
+ctx.fillStyle = "red";
+ctx.fillRect(120, 20, 100, 100);
+
+ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
+ctx.fillRect(210, 105, 100, 100);
+
+ctx.fillStyle = "green";
+ctx.fillRect(300, 190, 100, 100);
+
+ctx.strokeStyle = "black";  
+ctx.lineWidth = 2;
+ctx.strokeRect(390, 275, 100, 100);
+
+ctx.strokeStyle = "red";  
 ctx.lineWidth = 3;
-ctx.strokeRect(160 + moverHorizontal, 105, 100, 100);
+ctx.strokeRect(470, 355, 100, 100);
 
-ctx.fillStyle = "black";
-ctx.fillRect(210 + moverHorizontal, 155, 100, 100);  
-
-ctx.fillStyle = "rgba(31, 147, 241)";  
-ctx.fillRect(265 + moverHorizontal, 205, 100, 100);
-
-ctx.strokeStyle = "lightgreen";  
-ctx.lineWidth = 1;
-ctx.strokeRect(315 + moverHorizontal, 255, 100, 100);
+//PRUEBA IMAGEN
