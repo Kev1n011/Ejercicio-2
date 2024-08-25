22b535a35c75872c5e8a6735e15c1da2531d49d7
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2024-08-25 13:15:54 -0700

    Commit ejercicio 15
    -Agregada la función de mouseout que faltaba

--- operaciones.js
+++ operaciones.js
@@ -4,8 +4,8 @@ const ctx = canvas.getContext("2d");
 //CIRCULOS
 ctx.beginPath();
 ctx.arc(150, 400, 120, 0, 2 * Math.PI);
-ctx.fillStyle = "orange";             
-ctx.fill(); 
+ctx.fillStyle = "orange";
+ctx.fill();
 ctx.strokeStyle = "black";
 ctx.lineWidth = 2.5;
 ctx.stroke();
@@ -17,29 +17,27 @@ ctx.lineWidth = 2.5;
 ctx.stroke();
 
 ctx.beginPath();
-ctx.arc(290, 330, 120, .8, 2.4); // Arco para la sonrisa
-ctx.fillStyle = "black"; // Color de relleno
-ctx.fill(); // Rellenar la forma
-ctx.strokeStyle = "blue"; // Color de borde
-ctx.lineWidth = 3; // Ancho de la línea de borde
-ctx.stroke(); // Dibujar el borde
+ctx.arc(290, 330, 120, .8, 2.4);
+ctx.fillStyle = "black";
+ctx.fill();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 3;
+ctx.stroke();
 
 //TRIANGULOS
-   
-
 var moverLados = 100;
 ctx.beginPath();
-ctx.moveTo(320 + moverLados, 255);    
-ctx.lineTo(430 + moverLados, 150);   
-ctx.lineTo(320 + moverLados, 500);   
-ctx.closePath();      
-ctx.fillStyle = "rgba(0, 255, 255, 0.5)"; 
-ctx.fill();              
+ctx.moveTo(320 + moverLados, 255);
+ctx.lineTo(430 + moverLados, 150);
+ctx.lineTo(320 + moverLados, 500);
+ctx.closePath();
+ctx.fillStyle = "rgba(0, 255, 255, 0.5)";
+ctx.fill();
 ctx.strokeStyle = "red";
 ctx.lineWidth = 3;
-ctx.stroke();        
+ctx.stroke();
 
-          
+
 
 //CUADRADOS
 ctx.fillStyle = "red";
@@ -51,30 +49,30 @@ ctx.fillRect(210, 105, 100, 100);
 ctx.fillStyle = "green";
 ctx.fillRect(300, 190, 100, 100);
 
-ctx.strokeStyle = "black";  
+ctx.strokeStyle = "black";
 ctx.lineWidth = 2;
 ctx.strokeRect(390, 275, 100, 100);
 
-ctx.strokeStyle = "red";  
+ctx.strokeStyle = "red";
 ctx.lineWidth = 3;
 ctx.strokeRect(470, 355, 100, 100);
 
 //Texto
-ctx.font = "50px Georgia";   
-ctx.strokeStyle = "red";    
-ctx.lineWidth = 2;         
-ctx.strokeText("Hola mundo!", 500, 50); 
+ctx.font = "50px Georgia";
+ctx.strokeStyle = "red";
+ctx.lineWidth = 2;
+ctx.strokeText("Hola mundo!", 500, 50);
 
 ctx.font = "50px Georgia";
-ctx.fillStyle  ="blue";
+ctx.fillStyle = "blue";
 ctx.fillText("Hola mundo!", 500, 50);
 
 
 //Imagen
 var img = new Image();
 img.src = 'images.jpeg';
-img.onload = function() {
-    ctx.drawImage(img, 750, 250, 800, 100); 
+img.onload = function () {
+    ctx.drawImage(img, 750, 250, 800, 100);
 };
 
 //Gradients
@@ -87,8 +85,8 @@ grd.addColorStop(1, "yellow");
 ctx.fillStyle = grd;
 ctx.fillRect(20, 520, 150, 100);
 
-ctx.fillStyle = "blue";  
-ctx.fillRect(300, 20, 150, 100);  
+ctx.fillStyle = "blue";
+ctx.fillRect(300, 20, 150, 100);
 
 const grd2 = ctx.createRadialGradient(575, 370, 5, 575, 370, 100);
 grd2.addColorStop(0, "red");
@@ -98,35 +96,61 @@ grd2.addColorStop(1, "blue");
 ctx.fillStyle = grd2;
 ctx.fillRect(570, 340, 150, 100);
 
+
+//Eventos de mouse
+
 var figura = 1;
+document.getElementById("myCanvas").addEventListener("mouseout", function (e) {
+
+    switch (figura) {
+        case 1:
+            figura++;
+            break;
+        case 2:
+            figura--;
+            break;
+
+    }
 
 
+})
 
-document.getElementById("myCanvas").addEventListener("mousedown", function(e){
-    
-    let numeroRandom = Math.floor(Math.random() * 256); 
+document.getElementById("myCanvas").addEventListener("mousedown", function (e) {
+
+    let numeroRandom = Math.floor(Math.random() * 256);
     let numeroRandom2 = Math.floor(Math.random() * 256);
-    
-  
+
+
     console.log(e);
-    if (figura == 1){
+    if (figura == 1) {
         ctx.beginPath();
-        ctx.arc(e.x - 10, e.y - 10, 20, 0, 2 * Math.PI);
-        ctx.fillStyle = `rgba(${numeroRandom}, ${numeroRandom2}, 255, 0.8)`;            
-        ctx.fill(); 
+        ctx.arc(e.x - 10, e.y - 10, 90, 0, 2 * Math.PI);
+        ctx.fillStyle = `rgba(${numeroRandom}, ${numeroRandom2}, 255, 0.8)`;
+        ctx.fill();
         ctx.strokeStyle = "black";
         ctx.lineWidth = 0.5;
         ctx.stroke();
-        figura++;
 
-    }else{
+
+    } else {
         ctx.beginPath();
-        ctx.fillRect(e.x-50, e.y-50, 100, 100);
-        ctx.fillStyle = "red";
-        
+        ctx.fillStyle = `rgba(${numeroRandom}, ${numeroRandom2}, 255, 0.8)`;
+        ctx.fillRect(e.x - 50, e.y - 50, 100, 100);
         ctx.fill();
-        figura--;
 
     }
 
 })
+
+document.getElementById("myCanvas").addEventListener("mousemove", function (e) {
+    console.log("aaaa");
+    
+    ctx.beginPath();
+    ctx.arc(e.x - 10, e.y - 10, 5, 0, 2 * Math.PI);
+    ctx.fillStyle = `black`
+    ctx.fill();
+
+});
+
+
+
