33177e6bb22a3a2cb0b056d1f2b13414c42d770a
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2024-09-02 17:26:20 -0700

    Add files via upload

--- operaciones.js
+++ operaciones.js
@@ -1,124 +1,334 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-
-document.getElementById("myCanvas").addEventListener("mouseup", function (e) {
-    console.log("Coordenadas:", e.x, e.y);
-});
-
-//fondo del canvas
-ctx.fillStyle = '#78b2ff';
-ctx.fillRect(0, 0, canvas.width, canvas.height - 100);
-
-
-
-//Suelo
+//CIRCULOS
+/*
 ctx.beginPath();
-ctx.fillStyle = "#08ff1c";
-ctx.fillRect(0, 725, 1500, 25);
+ctx.arc(150, 400, 120, 0, 2 * Math.PI);
+ctx.fillStyle = "orange";
+ctx.fill();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
+ctx.stroke();
 
 ctx.beginPath();
-ctx.fillStyle = "#0aad18";
-ctx.fillRect(0, 750, 1500, 25);
+ctx.arc(290, 400, 120, 0, 2 * Math.PI);
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
+ctx.stroke();
 
 ctx.beginPath();
-ctx.fillStyle = "#03540a";
-ctx.fillRect(0, 775, 1500, 25);
+ctx.arc(290, 330, 120, .8, 2.4);
+ctx.fillStyle = "black";
+ctx.fill();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 3;
+ctx.stroke();
 
-//Casa
+//TRIANGULOS
+var moverLados = 100;
 ctx.beginPath();
-ctx.fillStyle = "#f7ff8a";
-ctx.fill()
-ctx.fillRect(500, 375, 550, 350);
+ctx.moveTo(320 + moverLados, 255);
+ctx.lineTo(430 + moverLados, 150);
+ctx.lineTo(320 + moverLados, 500);
+ctx.closePath();
+ctx.fillStyle = "rgba(0, 255, 255, 0.5)";
+ctx.fill();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 3;
+ctx.stroke();
+
+
+
+//CUADRADOS
+ctx.fillStyle = "red";
+ctx.fillRect(120, 20, 100, 100);
+
+ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
+ctx.fillRect(210, 105, 100, 100);
+
+ctx.fillStyle = "green";
+ctx.fillRect(300, 190, 100, 100);
+
 ctx.strokeStyle = "black";
 ctx.lineWidth = 2;
-ctx.strokeRect(500, 375, 550, 350);
+ctx.strokeRect(390, 275, 100, 100);
 
-//Puerta
-ctx.beginPath();
-ctx.fillStyle = "red";
-ctx.fill()
-ctx.fillRect(720, 525, 115, 200);
-ctx.strokeStyle = "black";
-ctx.lineWidth = 3.5;
-ctx.strokeRect(720, 525, 115, 200);
+ctx.strokeStyle = "red";
+ctx.lineWidth = 3;
+ctx.strokeRect(470, 355, 100, 100);
 
-//Techo
-ctx.beginPath();
-ctx.moveTo(400, 425);
-ctx.lineTo(1150, 425);
-ctx.lineTo(775, 100);  
-ctx.closePath();
-ctx.fillStyle = "red";  
-ctx.fill();              
-ctx.lineWidth = 2;         
-ctx.stroke();  
+//Texto
+ctx.font = "50px Georgia";
+ctx.strokeStyle = "red";
+ctx.lineWidth = 2;
+ctx.strokeText("Hola mundo!", 500, 50);
+
+ctx.font = "50px Georgia";
+ctx.fillStyle = "blue";
+ctx.fillText("Hola mundo!", 500, 50);
 
 
-ctx.stroke();
+//Imagen
+var img = new Image();
+img.src = 'images.jpeg';
+img.onload = function () {
+    ctx.drawImage(img, 750, 250, 800, 100);
+};
 
-function dibujar_ventana(x, y) {
+//Gradients
+
+
+const grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "yellow");
+
+ctx.fillStyle = grd;
+ctx.fillRect(20, 520, 150, 100);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(300, 20, 150, 100);
+
+const grd2 = ctx.createRadialGradient(575, 370, 5, 575, 370, 100);
+grd2.addColorStop(0, "red");
+grd2.addColorStop(1, "blue");
+
+
+ctx.fillStyle = grd2;
+ctx.fillRect(570, 340, 150, 100);
+
+
+//Eventos de mouse
+
+var figura = 1;
+document.getElementById("myCanvas").addEventListener("mouseout", function (e) {
+
+    switch (figura) {
+        case 1:
+            figura++;
+            break;
+        case 2:
+            figura--;
+            break;
+
+    }
+
+
+})
+var botonPresionado = false; 
+
+
+document.getElementById("myCanvas").addEventListener("mousedown", function (e) {
+
+    let numeroRandom = Math.floor(Math.random() * 256);
+    let numeroRandom2 = Math.floor(Math.random() * 256);
+    botonPresionado = true;
+
+
+    console.log(e);
+    if (figura == 1) {
+        ctx.beginPath();
+        ctx.arc(e.x - 10, e.y - 10, 90, 0, 2 * Math.PI);
+        ctx.fillStyle = `rgba(${numeroRandom}, ${numeroRandom2}, 255, 0.8)`;
+        ctx.fill();
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 0.5;
+        ctx.stroke();
+
+
+    } else {
+        ctx.beginPath();
+        ctx.fillStyle = `rgba(${numeroRandom}, ${numeroRandom2}, 255, 0.8)`;
+        ctx.fillRect(e.x - 50, e.y - 50, 100, 100);
+        ctx.fill();
+
+    }
+
+})
+
+document.getElementById("myCanvas").addEventListener("mouseup", function (e) {
+    botonPresionado = false; 
+});
+
+document.getElementById("myCanvas").addEventListener("mousemove", function (e) {
+    if (botonPresionado == true) { 
+        ctx.beginPath();
+        ctx.arc(e.x - 10, e.y - 10, 5, 0, 2 * Math.PI);
+        ctx.fillStyle = `black`;
+        ctx.fill();
+    }
+});*/
+
+var super_x = 100;
+var super_y = 100;
+var direccion = "";
+var velocidad = 5;
+
+class Rectangulo {
+    constructor(x, y, ancho, alto, color) {
+        this.x = x;
+        this.y = y;
+        this.ancho = ancho;
+        this.alto = alto;
+        this.color = color;
+    }
+
+    dibujar() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.ancho, this.alto);
+    }
+
+    colisionaCon(otro) {
+        return (
+            this.x < otro.x + otro.ancho &&
+            this.x + this.ancho > otro.x &&
+            this.y < otro.y + otro.alto &&
+            this.y + this.alto > otro.y
+        );
+    }
+    
+
+    teletransportar() {
+        this.x = Math.random() * (canvas.width - this.ancho);
+        this.y = Math.random() * (canvas.height - this.alto);
+    }
+
+    ajustarPosicion(direccion) {
+        switch (direccion) {
+            case "arriba":
+                this.y += velocidad;
+                break;
+            case "abajo":
+                this.y -= velocidad;
+                break;
+            case "izquierda":
+                this.x += velocidad;
+                break;
+            case "derecha":
+                this.x -= velocidad;
+                break;
+        }
+    }
+    
+}
+
+// Crear instancias de Rectangulo
+const player = new Rectangulo(super_x, super_y, 100, 100, "red");
+const cuadrado = new Rectangulo(230, 130, 100, 100, "rgba(0, 0, 255, 0.5)");
+const obstaculo = new Rectangulo(340, 240, 100, 100, "green")
+
+paint();
+
+document.addEventListener("keypress", function (e) {
+    var tecla_presionada = e.keyCode;
+    console.log("tecla: " + tecla_presionada)
+
+    //a = 97 s = 115 d = 100 w = 119
+    switch (tecla_presionada) {
+        case 97:
+            direccion = "izquierda";
+            break;
+        case 115:
+            direccion = "abajo";
+            break;
+        case 100:
+            direccion = "derecha";
+            break;
+        case 119:
+            direccion = "arriba";
+
+            break;
+        case 32:
+            velocidad += 10;
+
+            break;
+
+    }
+
+
+
+
+});
+
+function update() {
+    switch (direccion) {
+        case "arriba":
+
+
+            player.y -= velocidad;
+            if (player.y < -110) {
+                player.y = 800;
+
+            }
+            break;
+        case "abajo":
+            player.y += velocidad;
+            console.log(super_y);
+            if (player.y > 800) {
+                player.y = -100;
+
+            }
+            break;
+        case "izquierda":
+            player.x -= velocidad;
+            console.log(player.x);
+
+            if (player.x < -110) {
+                player.x = 1500
+
+            }
+            break;
+        case "derecha":
+            player.x += velocidad;
+            if (player.x > 1500) {
+                player.x = -100;
+
+            }
+
+
+    }
+    
+
+    if (player.colisionaCon(cuadrado)) {
+        cuadrado.teletransportar();
+    }
+
    
-    ctx.beginPath();
-    ctx.fillStyle = "white";
-    ctx.fillRect(x, y, 100, 100);
+    
 
-    // Primer stroke
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 4;
-    ctx.strokeRect(x, y, 100, 100); 
 
-    // Segundo stroke
-    ctx.lineWidth = 2;
-    ctx.strokeRect(x - 5, y - 5, 110, 110); 
-
-    //Diseño de cruz para la ventana
-    ctx.beginPath();
-    ctx.moveTo(x + 50, y); 
-    ctx.lineTo(x + 50, y + 100);
-    ctx.moveTo(x, y + 50); 
-    ctx.lineTo(x + 100, y + 50);
-    ctx.lineWidth = 4;
-    ctx.stroke();
 }
 
-dibujar_ventana(900, 450);
-dibujar_ventana(550,450);
+function paint() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-//Sol
-ctx.beginPath();
-ctx.arc(1300, 120, 90, 0, Math.PI * 2); 
-ctx.fillStyle = "yellow";
-ctx.fill();
-ctx.strokeStyle = "orange";
-ctx.lineWidth = 4;
-ctx.stroke();
 
-function dibujar_nube(x, y) {
-    ctx.beginPath();
-    ctx.arc(x, y, 30, Math.PI * 0.5, Math.PI * 1.5); 
-    ctx.arc(x + 40, y - 30, 40, Math.PI * 1, Math.PI * 2);
-    ctx.arc(x + 80, y, 30, Math.PI * 1.5, Math.PI * 0.5); 
-    ctx.arc(x + 40, y + 30, 30, Math.PI * 0, Math.PI * 1);
-    ctx.closePath();
-    ctx.fillStyle = "white";
-    ctx.fill();
-    ctx.strokeStyle = "lightgray";
-    ctx.lineWidth = 2;
-    ctx.stroke();
+
+    player.dibujar();
+    cuadrado.dibujar();
+    obstaculo.dibujar();
+    update();
+    requestAnimationFrame(paint)
+
+
 }
 
-dibujar_nube(200, 150);
-dibujar_nube(500, 100);
-dibujar_nube(1000,100);
+requestAnimationFrame(paint)
+
+window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 17);
+        };
+}());
 
 
 
+/*
+update
+req
 
 
-
-
-
-
-
-
+req*/
